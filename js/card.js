2cf201d6c6ebb33a40ecbda026bf9bf277fc616f
laptop_Maksim_Alekseev <maxim040597@gmail.com>
2024-08-30 23:28:28 +0300

    fix block

--- js/card.js
+++ js/card.js
@@ -1,2 +1,32 @@
-// const cards = document.querySelector('')
-console.log(products);
+const products = document.querySelector('#products');
+productsList;
+
+// console.log(productsList)
+products.innerHTML='';
+productsList.forEach(product => {
+    console.log(product.image)
+    products.innerHTML += `
+    <div class="products__card card">
+        <div class="card__img">
+            <img src="images/${product.image}" alt="id: ${product.id}">
+            <div class="card__btn">
+                <a href="product.html" class="card__cart">
+                    <div class="card__cart-wrap">
+                        <i class="fa-solid fa-bag-shopping"></i>
+                        <span>add to cart</span>	
+                    </div>
+                </a>
+                <a href="#" class="card__like">
+                    <i class="fa-regular fa-heart"></i>
+                    <!-- <i class="fa-solid fa-heart"></i> -->
+                </a>
+            </div>
+        </div>
+        <div class="card__text">
+            <h4>Flamboyant Pink Top</h4>
+            ${(product.price != null)? `<p class="card__price">$${(Math.round(product.price*100)/100).toFixed(2)}</p>`:''}
+        </div>
+    </div>`;
+});
+
+// console.log(products)
--- js/product.js
+++ js/product.js
@@ -1,16 +1,16 @@
-let products = [
+let productsList = [
     {
         'id':1,
         'title': 'Flamboyant Pink Top',
-        'images': 'product/1.jpg',
-        'price': 35.00,
+        'image': 'product/1.jpg',
+        'price': 35,
         'promo': null,
         'carousel': false
     },
     {
         'id':2,
         'title': null,
-        'images': null,
+        'image': 'product/2.jpg',
         'price': null,
         'promo': null,
         'carousel': false
@@ -18,7 +18,7 @@ let products = [
     {
         'id':3,
         'title': null,
-        'images': null,
+        'image': 'product/3.jpg',
         'price': null,
         'promo': null,
         'carousel': false
@@ -26,7 +26,7 @@ let products = [
     {
         'id':4,
         'title': null,
-        'images': null,
+        'image': 'product/4.jpg',
         'price': null,
         'promo': null,
         'carousel': false
@@ -34,7 +34,7 @@ let products = [
     {
         'id':5,
         'title': null,
-        'images': null,
+        'image': 'product/5.jpg',
         'price': null,
         'promo': null,
         'carousel': false
@@ -42,9 +42,58 @@ let products = [
     {
         'id':6,
         'title': null,
-        'images': null,
+        'image': 'product/6.jpg',
+        'price': null,
+        'promo': null,
+        'carousel': false
+    },
+    {
+        'id':7,
+        'title': null,
+        'image': 'product/7.jpg',
+        'price': null,
+        'promo': null,
+        'carousel': false
+    },
+    {
+        'id':8,
+        'title': null,
+        'image': 'product/8.jpg',
+        'price': null,
+        'promo': null,
+        'carousel': false
+    },
+    {
+        'id':9,
+        'title': null,
+        'image': 'product/9.jpg',
+        'price': null,
+        'promo': null,
+        'carousel': false
+    },
+    {
+        'id':10,
+        'title': null,
+        'image': 'product/10.jpg',
+        'price': null,
+        'promo': null,
+        'carousel': false
+    },
+    {
+        'id':11,
+        'title': null,
+        'image': 'product/11.jpg',
+        'price': null,
+        'promo': null,
+        'carousel': false
+    },
+    {
+        'id':12,
+        'title': null,
+        'image': 'product/12.jpg',
         'price': null,
         'promo': null,
         'carousel': false
     }
 ]
+
